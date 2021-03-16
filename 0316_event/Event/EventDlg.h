b60362597a6e33c60d87cf9d2834775e437ace70
chia56028 <chia56028@gmail.com>
2021-03-16 15:03:29 +0800

    initial

---
+++ 0316_event/Event/EventDlg.h
@@ -0,0 +1,34 @@
+﻿
+// EventDlg.h: 標頭檔
+//
+
+#pragma once
+
+
+// CEventDlg 對話方塊
+class CEventDlg : public CDialogEx
+{
+// 建構
+public:
+	CEventDlg(CWnd* pParent = nullptr);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EVENT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
