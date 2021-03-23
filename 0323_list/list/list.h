79b7c62e9d61a45024a4f60e48c3fa45c6af1466
chia56028 <chia56028@gmail.com>
2021-03-23 14:38:50 +0800

    init

---
+++ 0323_list/list/list.h
@@ -0,0 +1,32 @@
+﻿
+// list.h: PROJECT_NAME 應用程式的主要標頭檔
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主要符號
+
+
+// ClistApp:
+// 查看 list.cpp 以了解此類別的實作
+//
+
+class ClistApp : public CWinApp
+{
+public:
+	ClistApp();
+
+// 覆寫
+public:
+	virtual BOOL InitInstance();
+
+// 程式碼實作
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern ClistApp theApp;
