1b2c3847d029e0bb1799bdb4f22b8135ce99ba8a
chia56028 <chia56028@gmail.com>
2021-04-27 18:56:45 +0800

    init

---
+++ 0427_breakout/breakout/breakoutDlg.h
@@ -0,0 +1,34 @@
+
+// breakoutDlg.h : header file
+//
+
+#pragma once
+
+
+// CbreakoutDlg dialog
+class CbreakoutDlg : public CDialogEx
+{
+// Construction
+public:
+	CbreakoutDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BREAKOUT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
