d4263eb9dc17817854a4c163ef02d7547aa1f232
chia56028 <chia56028@gmail.com>
2021-05-11 13:58:41 +0800

    init

---
+++ 0511_UDP_chat/UDP_chat/UDP_chat.h
@@ -0,0 +1,32 @@
+﻿
+// UDP_chat.h: PROJECT_NAME 應用程式的主要標頭檔
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主要符號
+
+
+// CUDPchatApp:
+// 查看 UDP_chat.cpp 以了解此類別的實作
+//
+
+class CUDPchatApp : public CWinApp
+{
+public:
+	CUDPchatApp();
+
+// 覆寫
+public:
+	virtual BOOL InitInstance();
+
+// 程式碼實作
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CUDPchatApp theApp;
---
+++ 0511_UDP_chat/UDP_chat/UDP_chatDlg.h
@@ -0,0 +1,34 @@
+﻿
+// UDP_chatDlg.h: 標頭檔
+//
+
+#pragma once
+
+
+// CUDPchatDlg 對話方塊
+class CUDPchatDlg : public CDialogEx
+{
+// 建構
+public:
+	CUDPchatDlg(CWnd* pParent = nullptr);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_UDP_CHAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
